5684927f48913b50228b338111f5d60b79cb3ab8
Mike Torres <apolloproductionsdj@gmail.com>
2020-08-01 08:15:24 +0000

    added carrierwave for local posts

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,7 +1,7 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy, :edit]
   def index
-
+    @grams = Gram.all
   end
 
   def destroy
@@ -53,7 +53,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params 
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found) 
